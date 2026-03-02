ea420641d1f4d499e5e97595293f7be8eae075d7
Anshul kumar <anshulkumar6241@gmail.com>
2026-03-02 23:55:42 +0530

    add config

--- l2back/src/config/db.js
+++ l2back/src/config/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "anshulG8958",
   database: "l2db",
   waitForConnections: true,
   connectionLimit: 10,
