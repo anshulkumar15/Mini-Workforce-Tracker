731f55bd620e730973d107e321b990a20147a00d
Anshul kumar <anshulkumar6241@gmail.com>
2026-03-02 23:59:33 +0530

    add cors in server js

--- l2back/src/server.js
+++ l2back/src/server.js
@@ -12,8 +12,12 @@ dotenv.config();
 
 const app=express();
 
+// app.use(cors({
+//     origin:"http://localhost:5173"
+// }))
+
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:"https://baalcut.com"
 }))
 
 app.use(express.json());
